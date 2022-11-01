9f64f0343f93a4b58d42a18dca0e60fc253d5215
RoZeroZero <60973481+RoZeroZero@users.noreply.github.com>
2022-11-01 22:53:50 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   python/lab5/database.db
    	modified:   python/lab5/main.py

--- python/lab5/main.py
+++ python/lab5/main.py
@@ -2,5 +2,5 @@ import datetime
 from pattern import *
 with db:
     db.create_tables([Vehicle, Price]) #генерация объектов на основе классов
-    helicopter = Vehicle.create(name='Ка-50')
+    Vehicle.create(name='Ка-50')
     vehicles = Vehicle.select()
