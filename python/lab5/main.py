49e3f61c044ea3c4bc58896b5aecaa6d2201ebb8
RoZeroZero <60973481+RoZeroZero@users.noreply.github.com>
2022-11-01 22:58:58 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   python/lab5/database.db
    	modified:   python/lab5/main.py

--- python/lab5/main.py
+++ python/lab5/main.py
@@ -4,3 +4,5 @@ with db:
     db.create_tables([Vehicle, Price]) #генерация объектов на основе классов
     Vehicle.create(name='Ка-50')
     vehicles = Vehicle.select()
+    Vehicle.insert_many([{'name': '3-qaswed'}, {'name': '4-yhnbgt'}]).execute()
+
