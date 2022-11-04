774ced41fc6c59d8e4bdcf0fb77e548a988d826d
RoZeroZero <60973481+RoZeroZero@users.noreply.github.com>
2022-11-04 12:47:54 +0700

    Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   python/lab5/__pycache__/pattern.cpython-310.pyc
    	modified:   python/lab5/database.db
    	modified:   python/lab5/main.py
    	modified:   python/lab5/pattern.py

--- python/lab5/main.py
+++ python/lab5/main.py
@@ -5,13 +5,19 @@ i = 0
 with db:
     Vehicle.delete().execute()
     Price.delete().execute()
-    db.create_tables([Vehicle, Price])
+    Underground.delete().execute()
+    Aqua.delete().execute()
+
+    db.create_tables([Vehicle, Price, Underground, Aqua])
+
     Vehicle.insert([{'name': 'qaswed', 'type': 'ergwg', 'amount': 0},
                     {'name': '4-yhnbgt', 'type': 'sdgha', 'amount': 1}]).execute()
 
     while i < 5:
         i += 1
         Vehicle.create(name = 'vzuh' * i, type = 'vzuh', amount = i)
+        Underground.create(name = 'vzuh' * i, type = 'vzuh', amount = i)
+        Aqua.create(name = 'vzuh' * i, type = 'vzuh', amount = i)
 
     Price.insert([{'amount': 0, 'payment_date': datetime.date(2022,10,25), 'expense_id': 1},
                   {'amount': 100, 'payment_date': datetime.date(2022,10,25), 'expense_id': 2}]).execute()
--- python/lab5/pattern.py
+++ python/lab5/pattern.py
@@ -9,9 +9,12 @@ class BaseModel(Model):
         order_by = 'id'
 
 class Vehicle(BaseModel):
-    name = CharField()
-    type = CharField()
+    name = CharField(null = True)
+    type = CharField(null = True)
     amount = FloatField()
+    number = CharField(null = True)
+    elephant = CharField(null = True)
+    banana = CharField(null = True)
 
     class Meta:
         db_table = 'vehicles'
@@ -19,8 +22,33 @@ class Vehicle(BaseModel):
 
 class Price(BaseModel):
     amount = FloatField()
-    payment_date = DateField()
-    expense_id = ForeignKeyField(Vehicle)
+    payment_date = DateField(null = True)
+    bitter = CharField(null = True)
+    coffee = CharField(null = True)
+    apple = CharField(null = True)
+    expense_id = ForeignKeyField(Vehicle, null = True)
     
     class Meta:
         db_table = 'prices'
+
+class Underground(BaseModel):
+    amount = FloatField()
+    payment_date = DateField(null = True)
+    bitter = CharField(null = True)
+    coffee = CharField(null = True)
+    apple = CharField(null = True)
+    expense_id = ForeignKeyField(Vehicle, null = True)
+    
+    class Meta:
+        db_table = 'underground'
+
+class Aqua(BaseModel):
+    amount = FloatField()
+    payment_date = DateField(null = True)
+    bitter = CharField(null = True)
+    coffee = CharField(null = True)
+    apple = CharField(null = True)
+    expense_id = ForeignKeyField(Vehicle, null = True)
+    
+    class Meta:
+        db_table = 'aqua'
