d80cc876810dcec3ef3506a2f4f1e7cf69314811
RoZeroZero <60973481+RoZeroZero@users.noreply.github.com>
2022-11-04 12:29:40 +0700

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   python/lab5/database.db
    	modified:   python/lab5/main.py

--- python/lab5/main.py
+++ python/lab5/main.py
@@ -1,11 +1,20 @@
 import datetime
 from pattern import *
+i = 0
 
 with db:
     Vehicle.delete().execute()
     Price.delete().execute()
-    db.create_tables([Vehicle, Price]) #генерация объектов на основе классов
-    Vehicle.insert([{'name': '3-qaswed', 'type': 'ergwg', 'amount': 0},
+    db.create_tables([Vehicle, Price])
+    Vehicle.insert([{'name': 'qaswed', 'type': 'ergwg', 'amount': 0},
                     {'name': '4-yhnbgt', 'type': 'sdgha', 'amount': 1}]).execute()
+
+    while i < 5:
+        i += 1
+        Vehicle.create(name = 'vzuh' * i, type = 'vzuh', amount = i)
+
     Price.insert([{'amount': 0, 'payment_date': datetime.date(2022,10,25), 'expense_id': 1},
-                  {'amount': 100, 'payment_date': datetime.date(2022,10,25), 'expense_id': 2}]).execute()  
+                  {'amount': 100, 'payment_date': datetime.date(2022,10,25), 'expense_id': 2}]).execute()
+
+    for Vehicle in Vehicle.select().where((Vehicle.id < 3) | (Vehicle.id > 5)):
+        print(Vehicle.id, Vehicle.name)
