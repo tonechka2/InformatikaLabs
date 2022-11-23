97d1ba3713d09dc520307e50992c25ece96476eb
RoZeroZero <60973481+RoZeroZero@users.noreply.github.com>
2022-11-23 13:37:58 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	renamed:    webs/lab1/rozerozero/index.html -> lab1/rozerozero/index.html
    	renamed:    webs/lab1/rozerozero/ipsum.png -> lab1/rozerozero/ipsum.png
    	renamed:    webs/lab1/rozerozero/lorem.png -> lab1/rozerozero/lorem.png
    	renamed:    webs/lab1/rozerozero/style.css -> lab1/rozerozero/style.css
    	renamed:    webs/lab2/kate/basic.jpg -> lab2/kate/basic.jpg
    	renamed:    webs/lab2/kate/index.html -> lab2/kate/index.html
    	renamed:    webs/lab2/kate/style.css -> lab2/kate/style.css
    	renamed:    webs/lab2/nika/index.html -> lab2/nika/index.html
    	renamed:    webs/lab2/nika/pictures/baursak/baursak1.jpg -> lab2/nika/pictures/baursak/baursak1.jpg
    	renamed:    webs/lab2/nika/pictures/baursak/baursak2.jpg -> lab2/nika/pictures/baursak/baursak2.jpg
    	renamed:    webs/lab2/nika/pictures/baursak/baursak3.jpg -> lab2/nika/pictures/baursak/baursak3.jpg
    	renamed:    webs/lab2/nika/pictures/bbm/bbm3.jpg -> lab2/nika/pictures/bbm/bbm3.jpg
    	renamed:    webs/lab2/nika/pictures/kas/kas3.jpg -> lab2/nika/pictures/kas/kas3.jpg
    	renamed:    webs/lab2/nika/pictures/shmp/shmp3.jpg -> lab2/nika/pictures/shmp/shmp3.jpg
    	renamed:    webs/lab2/nika/pictures/sirne/sirne3.jpg -> lab2/nika/pictures/sirne/sirne3.jpg
    	renamed:    webs/lab2/nika/reset.css -> lab2/nika/reset.css
    	renamed:    webs/lab2/nika/style.css -> lab2/nika/style.css
    	renamed:    webs/lab2/nikita/basic.webp -> lab2/nikita/basic.webp
    	renamed:    webs/lab2/nikita/fine.webp -> lab2/nikita/fine.webp
    	renamed:    webs/lab2/nikita/index.html -> lab2/nikita/index.html
    	renamed:    webs/lab2/nikita/style.css -> lab2/nikita/style.css
    	renamed:    webs/lab2/rozerozero/basic.jpg -> lab2/rozerozero/basic.jpg
    	renamed:    webs/lab2/rozerozero/index.html -> lab2/rozerozero/index.html
    	renamed:    webs/lab2/rozerozero/ipsum.png -> lab2/rozerozero/ipsum.png
    	renamed:    webs/lab2/rozerozero/lorem.png -> lab2/rozerozero/lorem.png
    	renamed:    webs/lab2/rozerozero/style.css -> lab2/rozerozero/style.css
    	renamed:    webs/lab2/template.png -> lab2/template.png
    	renamed:    webs/lab3/rozerozero/index.html -> lab3/rozerozero/index.html
    	renamed:    webs/lab3/rozerozero/script.js -> lab3/rozerozero/script.js
    	renamed:    webs/lab3/rozerozero/style.css -> lab3/rozerozero/style.css
    	renamed:    python/lab4/nika/database.db -> lab4/nika/database.db
    	renamed:    python/lab4/nika/database_sqlite.py -> lab4/nika/database_sqlite.py
    	renamed:    python/lab4/rozerozero/database.db -> lab4/rozerozero/database.db
    	renamed:    python/lab4/rozerozero/database_sqlite.py -> lab4/rozerozero/database_sqlite.py
    	renamed:    python/lab5/fegay/main.py -> lab5/fegay/main.py
    	renamed:    python/lab5/fegay/pattern.py -> lab5/fegay/pattern.py
    	renamed:    python/lab5/nika/main.py -> lab5/nika/main.py
    	renamed:    python/lab5/nika/pattern.py -> lab5/nika/pattern.py
    	renamed:    python/lab5/rozerozero/__pycache__/pattern.cpython-310.pyc -> lab5/rozerozero/__pycache__/pattern.cpython-310.pyc
    	renamed:    python/lab5/rozerozero/database.db -> lab5/rozerozero/database.db
    	renamed:    python/lab5/rozerozero/main.py -> lab5/rozerozero/main.py
    	renamed:    python/lab5/rozerozero/pattern.py -> lab5/rozerozero/pattern.py
    	renamed:    python/lab6/nika/math.ipynb -> lab6/nika/math.ipynb
    	renamed:    python/lab6/rozerozero/math.ipynb -> lab6/rozerozero/math.ipynb
    	renamed:    python/lab7/rozerozero/math.py -> lab7/rozerozero/math.py
    	renamed:    python/lab8/lab.py -> lab8/lab.py

--- lab7/rozerozero/math.py
+++ lab7/rozerozero/math.py
@@ -12,16 +12,16 @@ def math_func():
     def calculation():
         f_eq_1 = 0
         temp = 0
-        f_eq_1_1 = (((var[1] + var[2])**2 + 1.08 * 10**3)**1/2) - var[3]
-        f_eq_1_2 = (((var[4] + var[2])**2 + 1.08 * 10**3)**1/2) - var[3]
-        f_eq_1_3 = (((var[5] + var[2])**2 + 1.08 * 10**3)**1/2) - var[3]
+        f_eq_1_1 = (((import_var()[1] + import_var()[2])**2 + 1.08 * 10**3)**1/2) - import_var()[3]
+        f_eq_1_2 = (((import_var()[4] + import_var()[2])**2 + 1.08 * 10**3)**1/2) - import_var()[3]
+        f_eq_1_3 = (((import_var()[5] + import_var()[2])**2 + 1.08 * 10**3)**1/2) - import_var()[3]
         while temp < 50:
             temp += 5
             if temp <= 11.4: f_eq_1 = f_eq_1_1
             elif ((temp > 11.4) or (temp <= 37.8)) : f_eq_1 = f_eq_1_2
             elif temp >  37.8: f_eq_1 = f_eq_1_3
-            f_eq_2 = 1250/((((temp + var[2])**2 + 1.08 * 10**3)**1/2) - (var[3] - 1))
-            p_eq.append(math.exp(var[0] * (2.68 * (1 - f_eq_2 / f_eq_1) - 1)))
+            f_eq_2 = 1250/((((temp + import_var()[2])**2 + 1.08 * 10**3)**1/2) - (import_var()[3] - 1))
+            p_eq.append(math.exp(import_var()[0] * (2.68 * (1 - f_eq_2 / f_eq_1) - 1)))
             print('temp:', temp, ' pressure:', p_eq[-1])
         return p_eq 
     import_var()
